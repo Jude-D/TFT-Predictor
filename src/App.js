d2ce0af52fbfa06b6947087d8a1b336d9e555634
Jude D'Souza <dsouza.jude936@gmail.com>
2021-05-05 23:56:02 -0400

    started project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { Button } from 'reactstrap';
+import React from 'react';
+import championData from './set 5/champions.json';
+import Images from './Images';
+
+function App() {
+	console.log(championData);
+	return (
+		<div className="Background container-fluid py-5">
+			<h1 className="Title d-flex justify-content-center">
+				Team Fight Tactics Composition Predictor
+			</h1>
+			<div className="BoardAndTraits">
+				<div className="SynergyAndClass col-3"></div>
+				<div className="Board col-9"></div>
+			</div>
+			<div className="Suggestion"></div>
+			<div className="Units col-9">
+				<ul className="ListOfChamps">
+					{championData.map((champion, id) => {
+						const { name, championId, cost, traits } = champion;
+						return (
+							<li key={id}>
+								<div>
+									<p>{name}</p>
+									<img src={Images[championId]} alt="" />
+									<p>{cost}</p>
+									<p>{traits}</p>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Images.js
@@ -0,0 +1,123 @@
+import TFT_TrainingDummy from './set 5/champions/TFT_TrainingDummy.png';
+import TFT5_Aatrox from './set 5/champions/TFT5_Aatrox.png';
+import TFT5_Aphelios from './set 5/champions/TFT5_Aphelios.png';
+import TFT5_Ashe from './set 5/champions/TFT5_Ashe.png';
+import TFT5_Brand from './set 5/champions/TFT5_Brand.png';
+import TFT5_Darius from './set 5/champions/TFT5_Darius.png';
+import TFT5_Diana from './set 5/champions/TFT5_Diana.png';
+import TFT5_Draven from './set 5/champions/TFT5_Draven.png';
+import TFT5_Garen from './set 5/champions/TFT5_Garen.png';
+import TFT5_Gragas from './set 5/champions/TFT5_Gragas.png';
+import TFT5_Hecarim from './set 5/champions/TFT5_Hecarim.png';
+import TFT5_Heimerdinger from './set 5/champions/TFT5_Heimerdinger.png';
+import TFT5_Ivern from './set 5/champions/TFT5_Ivern.png';
+import TFT5_Jax from './set 5/champions/TFT5_Jax.png';
+import TFT5_Kalista from './set 5/champions/TFT5_Kalista.png';
+import TFT5_Karma from './set 5/champions/TFT5_Karma.png';
+import TFT5_Katarina from './set 5/champions/TFT5_Katarina.png';
+import TFT5_Kayle from './set 5/champions/TFT5_Kayle.png';
+import TFT5_Kennen from './set 5/champions/TFT5_Kennen.png';
+import TFT5_Khazix from './set 5/champions/TFT5_Khazix.png';
+import TFT5_Kindred from './set 5/champions/TFT5_Kindred.png';
+import TFT5_Kled from './set 5/champions/TFT5_Kled.png';
+import TFT5_LeBlanc from './set 5/champions/TFT5_LeBlanc.png';
+import TFT5_LeeSin from './set 5/champions/TFT5_LeeSin.png';
+import TFT5_Leona from './set 5/champions/TFT5_Leona.png';
+import TFT5_Lissandra from './set 5/champions/TFT5_Lissandra.png';
+import TFT5_Lulu from './set 5/champions/TFT5_Lulu.png';
+import TFT5_Lux from './set 5/champions/TFT5_Lux.png';
+import TFT5_Mordekaiser from './set 5/champions/TFT5_Mordekaiser.png';
+import TFT5_Morgana from './set 5/champions/TFT5_Morgana.png';
+import TFT5_Nautilus from './set 5/champions/TFT5_Nautilus.png';
+import TFT5_Nidalee from './set 5/champions/TFT5_Nidalee.png';
+import TFT5_Nocturne from './set 5/champions/TFT5_Nocturne.png';
+import TFT5_Nunu from './set 5/champions/TFT5_Nunu.png';
+import TFT5_Pantheon from './set 5/champions/TFT5_Pantheon.png';
+import TFT5_Poppy from './set 5/champions/TFT5_Poppy.png';
+import TFT5_Rell from './set 5/champions/TFT5_Rell.png';
+import TFT5_Riven from './set 5/champions/TFT5_Riven.png';
+import TFT5_Ryze from './set 5/champions/TFT5_Ryze.png';
+import TFT5_Sejuani from './set 5/champions/TFT5_Sejuani.png';
+import TFT5_Sett from './set 5/champions/TFT5_Sett.png';
+import TFT5_Soraka from './set 5/champions/TFT5_Soraka.png';
+import TFT5_Syndra from './set 5/champions/TFT5_Syndra.png';
+import TFT5_Taric from './set 5/champions/TFT5_Taric.png';
+import TFT5_Teemo from './set 5/champions/TFT5_Teemo.png';
+import TFT5_Thresh from './set 5/champions/TFT5_Thresh.png';
+import TFT5_Trundle from './set 5/champions/TFT5_Trundle.png';
+import TFT5_Udyr from './set 5/champions/TFT5_Udyr.png';
+import TFT5_Varus from './set 5/champions/TFT5_Varus.png';
+import TFT5_Vayne from './set 5/champions/TFT5_Vayne.png';
+import TFT5_Velkoz from './set 5/champions/TFT5_Velkoz.png';
+import TFT5_Viego from './set 5/champions/TFT5_Viego.png';
+import TFT5_Viktor from './set 5/champions/TFT5_Viktor.png';
+import TFT5_Vladimir from './set 5/champions/TFT5_Vladimir.png';
+import TFT5_Volibear from './set 5/champions/TFT5_Volibear.png';
+import TFT5_Warwick from './set 5/champions/TFT5_Warwick.png';
+import TFT5_Yasuo from './set 5/champions/TFT5_Yasuo.png';
+import TFT5_Ziggs from './set 5/champions/TFT5_Ziggs.png';
+import TFT5_Zyra from './set 5/champions/TFT5_Zyra.png';
+
+const Images = {
+	TFT_TrainingDummy,
+	TFT5_Aatrox,
+	TFT5_Aphelios,
+	TFT5_Ashe,
+	TFT5_Brand,
+	TFT5_Darius,
+	TFT5_Diana,
+	TFT5_Draven,
+	TFT5_Garen,
+	TFT5_Gragas,
+	TFT5_Hecarim,
+	TFT5_Heimerdinger,
+	TFT5_Ivern,
+	TFT5_Jax,
+	TFT5_Kalista,
+	TFT5_Karma,
+	TFT5_Katarina,
+	TFT5_Kayle,
+	TFT5_Kennen,
+	TFT5_Khazix,
+	TFT5_Kindred,
+	TFT5_Kled,
+	TFT5_LeBlanc,
+	TFT5_LeeSin,
+	TFT5_Leona,
+	TFT5_Lissandra,
+	TFT5_Lulu,
+	TFT5_Lux,
+	TFT5_Mordekaiser,
+	TFT5_Morgana,
+	TFT5_Nautilus,
+	TFT5_Nidalee,
+	TFT5_Nocturne,
+	TFT5_Nunu,
+	TFT5_Pantheon,
+	TFT5_Poppy,
+	TFT5_Rell,
+	TFT5_Riven,
+	TFT5_Ryze,
+	TFT5_Sejuani,
+	TFT5_Sett,
+	TFT5_Soraka,
+	TFT5_Syndra,
+	TFT5_Taric,
+	TFT5_Teemo,
+	TFT5_Thresh,
+	TFT5_Trundle,
+	TFT5_Udyr,
+	TFT5_Varus,
+	TFT5_Vayne,
+	TFT5_Velkoz,
+	TFT5_Viego,
+	TFT5_Viktor,
+	TFT5_Vladimir,
+	TFT5_Volibear,
+	TFT5_Warwick,
+	TFT5_Yasuo,
+	TFT5_Ziggs,
+	TFT5_Zyra,
+};
+
+export default Images;
