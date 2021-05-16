134ba928dbc3910f4d728ed7374b2bd6cfd051ae
Jude D'Souza <dsouza.jude936@gmail.com>
2021-05-15 21:50:44 -0400

    added some class names for further styling

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 					{championData.map((champion, id) => {
 						const { name, championId, cost, traits } = champion;
 						return (
-							<Card>
+							<Card className="Champion Cards">
 								<li key={id}>
 									<div>
 										<CardTitle>{name}</CardTitle>
